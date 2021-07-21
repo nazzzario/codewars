40e98b2415f61e2a3bb550dcf2b0591a6f21c988
Nazar Krasnovoronka <krasnovoronka@pc-1172.ca.mns.gov.ua>
2021-07-21 12:53:53 +0300

    add some tasks

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+j
+:src/com/shpp/p2p/cs/nkrasnovoronka/assignment14/Task1.java,2/c/2c014239d3db211b56f9af001841a3c81908d2b1
---
+++ src/com/shpp/p2p/cs/nkrasnovoronka/assignment14/Task14.java
@@ -0,0 +1,30 @@
+package com.shpp.p2p.cs.nkrasnovoronka.assignment14;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+//Moves in squared strings (I)
+public class Task14 {
+
+    public static void main(String[] args) {
+       String s = "abcd\nefgh\nijkl\nmnop";
+        String s1 = vertMirror(s);
+        System.out.println(s1);
+        System.out.println(horMirror(s));
+    }
+    public static String vertMirror (String strng) {
+        List<String> strings = Arrays.asList(strng.split("\n"));
+        return strings.stream().map(s -> new StringBuilder(s).reverse().toString()).collect(Collectors.joining("\n"));
+    }
+    public static String horMirror (String strng) {
+        List<String> strings = Arrays.asList(strng.split("\n"));
+        Collections.reverse(strings);
+        return String.join("\n", strings);
+    }
+    public static String oper(Function<String, String> operator, String s) {
+        return operator.apply(s);
+    }
+}
