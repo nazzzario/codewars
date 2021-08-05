28ef68bbb5746069b0d0559fb9ff2ade29ce1e57
krasnovoronka <nazzzario23@gmaill.com>
2021-08-05 15:59:17 +0300

    improve task

--- src/com/shpp/p2p/cs/nkrasnovoronka/assignment14/threads/ListCount.java
+++ src/com/shpp/p2p/cs/nkrasnovoronka/assignment14/threads/ListCount.java
@@ -1,25 +1,36 @@
 package com.shpp.p2p.cs.nkrasnovoronka.assignment14.threads;
 
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ListCount {
     public static void main(String[] args) {
         List<Integer> primeNumbers = Stream.of(1,2,3,4,5,6,7,8,9,10).collect(Collectors.toList());
-        MyThread leftPart = new MyThread(primeNumbers.subList(0, primeNumbers.size()/ 2), "leftPart");
-        MyThread rightPart = new MyThread(primeNumbers.subList(primeNumbers.size()/ 2, primeNumbers.size()), "rightPart");
+        FutureTask<Integer> leftPart = new FutureTask<>(new MyThread(primeNumbers.subList(0, primeNumbers.size()/ 2)));
+        FutureTask<Integer> rightPart =  new FutureTask<>(new MyThread(primeNumbers.subList(primeNumbers.size()/ 2, primeNumbers.size())));
 
-        leftPart.start();
-        rightPart.start();
+        Thread t1 = new Thread(leftPart);
+        Thread t2 = new Thread(rightPart);
+
+        t1.start();
+        t2.start();
         try {
-            leftPart.join();
-            rightPart.join();
+            t1.join();
+            t2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        int res = leftPart.getPrimeNumbers() + rightPart.getPrimeNumbers();
+        int res = 0;
+        try {
+            res = leftPart.get() + rightPart.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
         System.out.println("Total number of primes: " + res);
 
 
@@ -40,32 +51,21 @@ public class ListCount {
     }
 }
 
-class MyThread extends Thread{
-    private String name;
-    private List<Integer> numbers;
-    private int primeNumbers;
-    public MyThread(List<Integer> numbers, String name) {
+class MyThread implements Callable<Integer> {
+    private final List<Integer> numbers;
+
+    public MyThread(List<Integer> numbers) {
         this.numbers = numbers;
-        this.name = name;
     }
-
+    
     @Override
-    public void run() {
-        System.out.println("Starting thread: " + name);
-        calculatePrimeNumbers();
-        System.out.println(primeNumbers);
-    }
-
-    private void calculatePrimeNumbers(){
+    public Integer call() {
+        int primeNumbers = 0;
         for (int i: numbers) {
             if(ListCount.isPrime(i)){
                 primeNumbers++;
             }
         }
-    }
-
-
-    public int getPrimeNumbers() {
         return primeNumbers;
     }
 }
