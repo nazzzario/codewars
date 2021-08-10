6b194570651f08bfa81a2bfe2661ea3dc2b96203
krasnovoronka <nazzzario23@gmaill.com>
2021-08-10 09:54:05 +0300

    create horse entity

--- src/com/shpp/p2p/cs/nkrasnovoronka/assignment14/race/Horse.java
+++ src/com/shpp/p2p/cs/nkrasnovoronka/assignment14/race/Horse.java
@@ -1,9 +1,12 @@
 package com.shpp.p2p.cs.nkrasnovoronka.assignment14.race;
 
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Phaser;
 
-public class Horse implements Runnable{
-    private String name;
+public class Horse implements Callable<Horse> {
+    private final String name;
+    private Integer distance;
 
     private final int FINAL_DISTANCE = 1000;
 
@@ -12,26 +15,41 @@ public class Horse implements Runnable{
 
     private final int MIN_SLEEP = 400;
     private final int MAX_SLEEP = 500;
+
+    private Phaser phaser;
+
     private final Random random;
 
 
     public Horse(String name) {
         this.name = name;
+        distance = 0;
         random = new Random();
+        phaser.register();
     }
 
     @Override
-    public void run() {
-
+    public Horse call() {
+        phaser.arriveAndAwaitAdvance();
+        while (distance < FINAL_DISTANCE){
+            move();
+            sleep();
+        }
+        phaser.arriveAndDeregister();
+        return this;
     }
 
+    private void move(){
+        distance = distance + generateDistance();
+    }
 
-
-
-
-
-
-
+    private void sleep(){
+        try {
+            Thread.sleep(generateSleep());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
     private int generateDistance(){
         return random.nextInt(MAX_DISTANCE - MIN_DISTANCE) + MIN_DISTANCE;
@@ -40,5 +58,13 @@ public class Horse implements Runnable{
     private int generateSleep(){
         return random.nextInt(MAX_SLEEP - MIN_SLEEP) + MIN_SLEEP;
     }
+
+    @Override
+    public String toString() {
+        return "Horse{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
 
 }
--- src/com/shpp/p2p/cs/nkrasnovoronka/assignment14/race/Main.java
+++ src/com/shpp/p2p/cs/nkrasnovoronka/assignment14/race/Main.java
@@ -1,4 +1,15 @@
 package com.shpp.p2p.cs.nkrasnovoronka.assignment14.race;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Main {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        List<Future<Horse>> hippodrome = new ArrayList<>();
+    }
 }
